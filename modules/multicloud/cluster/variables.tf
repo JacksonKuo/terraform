7b501e3f93bc2edcb2c7213650bd3d99110377da
JacksonKuo <JacksonKuo@users.noreply.github.com>
2025-05-10 12:28:58 -0500

    provider validation

--- modules/multicloud/cluster/variables.tf
+++ modules/multicloud/cluster/variables.tf
@@ -1,4 +1,9 @@
 variable "cloud_provider" {
     type      = string
     default   = "digitalocean"
+
+    validation {
+        condition     = contains(["digitalocean"], var.cloud_provider)
+        error_message = "Environment must to a valid cloud provider"
+    }
 }
