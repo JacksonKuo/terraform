5bd9ce7e9c8710eec901effe475257c7c2446918
JacksonKuo <JacksonKuo@users.noreply.github.com>
2025-05-11 17:07:01 -0500

    update

--- modules/multicloud/cluster/main.tf
+++ modules/multicloud/cluster/main.tf
@@ -7,22 +7,22 @@ module do_droplet {
     }
 }
 
-module cf_dns {
-    count = var.cloud_provider == "digitalocean" ? 1 : 0
-    source = "../../cloudflare/dns"
-    droplet_ip = module.do_droplet[0].droplet_ip
-    providers = {
-        cloudflare.child = cloudflare.parent
-    }
-    depends_on = [module.do_droplet]
-}
-
 module gh_secrets {
     count = var.cloud_provider == "digitalocean" ? 1 : 0
     source = "../../github/secrets"
     droplet_ip = module.do_droplet[0].droplet_ip
     providers = {
         github.child = github.parent
+    }
+    depends_on = [module.do_droplet]
+}
+
+module cf_dns {
+    count = var.cloud_provider == "digitalocean" ? 1 : 0
+    source = "../../cloudflare/dns"
+    droplet_ip = module.do_droplet[0].droplet_ip
+    providers = {
+        cloudflare.child = cloudflare.parent
     }
     depends_on = [module.do_droplet]
 }
